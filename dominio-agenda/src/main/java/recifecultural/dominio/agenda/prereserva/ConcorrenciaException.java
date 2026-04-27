e8849c3643794a077cd116f6e83c0b79b01738cc
rafael-zzz <rmp2@cesar.school>
2026-04-27 12:12:10 -0300

    feat: adição de domínio artista e subdomínios setor e prereserva, dentro de agenda

--- dominio-agenda/src/main/java/recifecultural/dominio/agenda/prereserva/ConcorrenciaException.java
+++
@@ -1,7 +0,0 @@
-package recifecultural.dominio.agenda.prereserva;
-
-public class ConcorrenciaException extends RuntimeException {
-    public ConcorrenciaException(String codigoAssento) {
-        super("O assento " + codigoAssento + " foi reservado por outro usuário. Tente outro assento.");
-    }
-}
--- dominio-agenda/src/main/java/recifecultural/dominio/agenda/prereserva/StatusPreReserva.java
+++
@@ -1,3 +0,0 @@
-package recifecultural.dominio.agenda.prereserva;
-
-public enum StatusPreReserva { ATIVA, EXPIRADA, CONVERTIDA, CANCELADA }
